bbdf0aaabd19103167b8327665260f37bb053bb4
Delilah1004 <pskdy8414@naver.com>
2021-03-23 18:04:14 +0900

    check box checked 기능 진행중

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -1,47 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ScrollView} from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import data from '../data.json';
-import Item from '../components/Item';
+import data from '../data.json'
+import Item from '../components/Item'
 import Loading from '../components/Loading';
 
 export default function MainPage() {
   console.disableYellowBox = true;
-
+  
   const [state, setState] = useState([])
+
   const [ready, setReady] = useState(true)
 
-  useEffect(() => {
-    // 뒤의 1000 숫자는 1초를 뜻함
-    // 1초 뒤에 실행되는 코드들이 담겨있는 함수
-    setTimeout(() => {
-      let item = data.item;
-      setState(item)
-      setCateState(item)
-      setReady(false)
-    }, 2000)
-  }, [])
+  useEffect(()=>{
+	   
+		//뒤의 1000 숫자는 1초를 뜻함
+    //1초 뒤에 실행되는 코드들이 담겨 있는 함수
+    setTimeout(()=>{
+        setState(data)
+        setReady(false)
+    },1000)
+  },[])
 
-  // 삼항연산자
-  // 처음 ready 상태값은 true 이므로 Loading 이 반환됨
-  // useEffect로 인해 데이터가 준비되고, ready 값이 변경되면 : 콜론 뒤의 값이 반환
-  return ready ? <Loading /> : (
-    <ScrollView style={styles.container}>
-      <StatusBar/>
-      <View>
+  let tasks = state.task;
+
+  const onToggle = key => e => {
+    setState(
+      tasks.map(task =>
+        task.key === key ? {...task, checked: !task.checked} : task,
+      ),
+    );
+  };
+
+  return ready ? <Loading/> : (
+    <ScrollView style={styles.scrollView}>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text style={styles.title}>To do List</Text>
         {
-          state.map((content, i) => {
-            return (<Item content={content} key={i} />)
+          tasks.map((task, i) => {
+            return (
+              <Item task={task} key={i} onToggle={onToggle}/>
+            )
           })
         }
       </View>
     </ScrollView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    paddingHorizontal: 10
+  scrollView: {
+    backgroundColor: '#262C74',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    marginTop: 50
   },
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 40,
+    borderRadius: 10
+  },
+  title: {
+    marginVertical: 10,
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center'
+  }
+})
