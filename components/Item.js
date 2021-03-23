7eb8b2d06e5b63278f059b1e9162589091125e91
Delilah1004 <pskdy8414@naver.com>
2021-03-23 23:43:32 +0900

    checkbox 성공

--- components/Item.js
+++ components/Item.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
-import CheckBox from 'react-native-check-box'
-import { StyleSheet, View } from 'react-native';
+// import CheckBox from 'react-native-check-box'
+import { Checkbox } from 'react-native-paper';
+import { StyleSheet, View, Text } from 'react-native';
 
 //비구조 할당 방식으로 넘긴 속성 데이터를 꺼내 사용함
 export default function Item({ task, onToggle, key }) {
+
+  const [checked, setChecked] = useState(task.checked);
+
   return (
     <View
       style={styles.item}>
-      <CheckBox
-        onClick={onToggle(key)}
-        isChecked={task.checked}
-        rightText={task.title}
+      <Checkbox
+        status={checked ? 'checked' : 'unchecked'}
+        onPress={() => {
+          setChecked(!checked);
+        }}
+        color="#262C74"
       />
+      <Text 
+        style={[checked ? styles.strikeText : styles.unstrikeText]}>{task.title}</Text>
     </View>
   )
 }
@@ -19,8 +27,17 @@ export default function Item({ task, onToggle, key }) {
 const styles = StyleSheet.create({
   item: {
     flex: 1,
+    flexDirection: "row",
+    paddingVertical: 10,
     borderBottomWidth: 0.5,
     borderBottomColor: '#eee',
-    paddingVertical: 10
+    alignItems: "center"
+  },
+  strikeText: {
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+  },
+  unstrikeText: {
+    color: '#29323c',
   }
 })
